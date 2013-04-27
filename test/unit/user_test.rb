e7c74745fcfb12a967aa38b8c6015055c65d7714
Riderman Barbosa <ridermansb@bindsolution.com>
2013-04-27 10:51:32 -0400

    Testing started

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enterr a firts name" do
+      user  = User.new
+      assert !user.save
+      assert !user.errors[:first_name].empty?
+  end
+  
+  
+  test "a user should enterr a last name" do
+      user  = User.new
+      assert !user.save
+      assert !user.errors[:last_name].empty?
+  end
+  
+  test "a user should enterr a profile name" do
+      user  = User.new
+      assert !user.save
+      assert !user.errors[:profile_name].empty?
+  end
+  
+  
 end
